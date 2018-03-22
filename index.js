041f826e120806e467fd38fc2331fc57a56dce26
Taylor August Giles <augustgiles8@gmail.com>
2018-03-22 14:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newObject = {itemName: item, itemPrice: Math.floor(Math.random() * (101 - 0) + 0)};
+  cart.push(newObject);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var cartArray = [];
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+  } else {
+    for (var n = 0; n < cart.length; n++) {
+      cartArray.push(` ${cart[n].itemName} at ${cart[n].itemPrice}`);
+      [...cartArray.slice(0, n),` and`, ...cartArray.slice(n)];
+    } return `In your cart, you have ${cartArray}.`;
+  }
 }
 
 function total() {
